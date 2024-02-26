b87d82adc4d849f1c12dc2144f52cbf1168b5bf5
csvinhal <cristiano.vinhal10@gmail.com>
2024-02-26 14:51:42 -0300

    add link to header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 import Logo from "../../../public/berliner_polizei.svg";
 
 const Header = () => {
   return (
     <div className="flex items-center mb-10">
-      <Image className="h-min" alt="Logo" src={Logo} height={80} width={80} />
+      <Link href="/" aria-label="Go to homepage">
+        <Image className="h-min" alt="Logo" src={Logo} height={80} width={80} />
+      </Link>
       <div className="pl-10">
         <h1 className="font-extrabold">Police Department of Berlin</h1>
         <h2 className="font-bold">Stolen Bikes</h2>
